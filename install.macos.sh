2648aaa0cdfa2d6edb66db8fd69cf07ccd1f313a
Sanjay Seshan <sanjay.seshan@outlook.com>
2020-04-12 17:21:57 -0400

    Create install.macos.sh

---
+++ install.macos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# delete from here (to disable the root check)
+if [ `whoami` == 'root' ]
+  then
+    echo "You cannot be root to do this. If you are running vscode as root for some reason, edit this file and delete the denoted region"
+    exit
+fi
+# to here
+
+cp -prv spiketools/ ~/
+cd ~/spiketools/
+pip3 install -r requirements.txt
+echo "All done!"

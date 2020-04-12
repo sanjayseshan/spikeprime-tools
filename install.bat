2cf91c1a7ff04843afc0a20bd8133b0e73861244
Sanjay Seshan <sanjay.seshan@outlook.com>
2020-04-12 15:26:19 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,7 +5,6 @@ ECHO Installing the SPIKE PRIME tools for VS CODE....
 PAUSE
 mkdir C:\spiketools
 xcopy /E /I spiketools\* C:\spiketools
-xcopy %appdata%\..\Local\Programs\Python\Python38-32\python.exe %appdata%\..\Local\Programs\Python\Python38-32\python3.exe
 cd C:\spiketools
 pip3 install -r requirements.txt
 PAUSE

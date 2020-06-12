7824b1b92f840578399fac7d82ef84d70d5dc2f1
Sanjay Seshan <sanjay.seshan@outlook.com>
2020-06-12 12:12:38 -0400

    use old api

--- spiketools/spikejsonrpc.py
+++ spiketools/spikejsonrpc.py
@@ -75,7 +75,7 @@ class RPC:
     return self.send_message('get_storage_status')
 
   def start_write_program(self, name, size, slot, created, modified):
-    meta = {'created': created, 'modified': modified, 'name': name, 'type': 'python', 'project_id': '50uN1ZaRpHj2'}
+    meta = {'created': created, 'modified': modified, 'name': name }
     return self.send_message('start_write_program', {'slotid':slot, 'size': size, 'meta': meta})
 
   def write_package(self, data, transferid):
